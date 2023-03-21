73a2c3d1fe2ad063eb88cfea501db2fadce34cd3
Faisal Rashid <faisalrashid9775@gmail.com>
2023-03-21 13:04:25 +0500

    Final2

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -5,15 +5,20 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div>
-      <Box bgColor={"teal.600"} color={'white'} p={{base : 5, lg :15}}>
-        <Container maxW={1400}  >
-          <Flex gap={{base : 12 , lg :24}} display={{base: 'grid' , lg :'flex'}} textAlign={{base: 'center' , lg: 'initial'}} py={{base : 2, lg : 5}}>
+      <Box bgColor={"teal.600"} color={"white"} p={{ base: 5, lg: 15 }}>
+        <Container maxW={1400}>
+          <Flex
+            gap={{ base: 12, lg: 24 }}
+            display={{ base: "grid", lg: "flex" }}
+            textAlign={{ base: "center", lg: "initial" }}
+            py={{ base: 2, lg: 5 }}
+          >
             <Box>
               <Heading>Navigation</Heading>
-              <Flex direction={'column'}>
-              <Link href={"/"}>Home</Link>
-              <Link href={"https://portal.piaic.org/signup"}>Apply</Link>
-              <Link href={"/HowitWork"}>How it Work</Link>
+              <Flex direction={"column"}>
+                <Link href={"/"}>Home</Link>
+                <Link href={"https://portal.piaic.org/signup"}>Apply</Link>
+                <Link href={"/HowitWork"}>How it Work</Link>
               </Flex>
             </Box>
             <Box>
@@ -26,16 +31,16 @@ export default function Footer() {
               </Flex>
             </Box>
             <Box>
-            <Heading>Contact us</Heading>
-                <Flex direction={'column'}>
-              <Link href={""}>Facebook</Link>
-              <Link href={""}>Youtube</Link>
-              <Link href={""}>Linkdln</Link>
-                </Flex>
+              <Heading>Contact us</Heading>
+              <Flex direction={"column"}>
+                <Link href={""}>Facebook</Link>
+                <Link href={""}>Youtube</Link>
+                <Link href={""}>Linkdln</Link>
+              </Flex>
             </Box>
             <Box>
-                <Heading>Help line</Heading>
-                <Text>+92-308-2220203 (WhatsApp as well)</Text>
+              <Heading>Help line</Heading>
+              <Text>+92-308-2220203 (WhatsApp as well)</Text>
             </Box>
           </Flex>
         </Container>
